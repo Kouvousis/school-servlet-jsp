113d0d2b2aeb4649d8eb876c81e65745078a53bf
Kouvousis <dexous1996@gmail.com>
2024-09-10 21:51:47 +0300

    teacher roles added

--- src/main/java/gr/aueb/cf/schoolapp/dao/UserDAOImpl.java
+++ src/main/java/gr/aueb/cf/schoolapp/dao/UserDAOImpl.java
@@ -1,5 +1,6 @@
 package gr.aueb.cf.schoolapp.dao;
 
+import gr.aueb.cf.schoolapp.core.RoleType;
 import gr.aueb.cf.schoolapp.dao.exceptions.UserDAOException;
 import gr.aueb.cf.schoolapp.model.User;
 import gr.aueb.cf.schoolapp.security.SecUtil;
@@ -13,7 +14,7 @@ import java.sql.SQLException;
 public class UserDAOImpl implements IUserDAO {
     @Override
     public User insertUser(User user) throws UserDAOException {
-        String sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+        String sql = "INSERT INTO users (username, password, role) VALUES (?, ?, ?)";
 
         try (Connection connection = DBUtil.getConnection();
              PreparedStatement ps = connection.prepareStatement(sql)) {
@@ -21,9 +22,11 @@ public class UserDAOImpl implements IUserDAO {
             // Extract model info
             String username = user.getUsername();
             String password = user.getPassword();
+            RoleType role = user.getRoleType();
 
             ps.setString(1, username);
             ps.setString(2, SecUtil.hashPassword(password));
+            ps.setString(3, role.name());
 
             ps.executeUpdate();
             // logging
@@ -50,7 +53,8 @@ public class UserDAOImpl implements IUserDAO {
             if (rs.next()) {
                 user = new User(rs.getInt("id"),
                         rs.getString("username"),
-                        rs.getString("password"));
+                        rs.getString("password"),
+                        RoleType.valueOf(rs.getString("role")));
             }
 
             return user;
@@ -76,7 +80,8 @@ public class UserDAOImpl implements IUserDAO {
             if (rs.next()) {
                 user = new User(rs.getInt("id"),
                         rs.getString("username"),
-                        rs.getString("password"));
+                        rs.getString("password"),
+                        RoleType.valueOf(rs.getString("role")));    // we assume that DB role is not null
             } else {
                 return false;
             }
